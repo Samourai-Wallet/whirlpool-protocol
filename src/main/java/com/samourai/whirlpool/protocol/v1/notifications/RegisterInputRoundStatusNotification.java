4d1304461760440b246bd23c259c58831df67f0b
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2018-05-10 16:55:25 +0200

    WhirlpoolClient reads denomination and minerFee from server

--- src/main/java/com/samourai/whirlpool/protocol/v1/notifications/RegisterInputRoundStatusNotification.java
+++ src/main/java/com/samourai/whirlpool/protocol/v1/notifications/RegisterInputRoundStatusNotification.java
@@ -1,6 +1,8 @@
 package com.samourai.whirlpool.protocol.v1.notifications;
 
 public class RegisterInputRoundStatusNotification extends RoundStatusNotification {
+    private long denomination;
+    private long minerFee;
 
     public RegisterInputRoundStatusNotification() {
 
@@ -8,5 +10,21 @@ public class RegisterInputRoundStatusNotification extends RoundStatusNotificatio
 
     public RegisterInputRoundStatusNotification(String roundId) {
         super(RoundStatus.REGISTER_INPUT, roundId);
+    }
+
+    public long getDenomination() {
+        return denomination;
+    }
+
+    public void setDenomination(long denomination) {
+        this.denomination = denomination;
+    }
+
+    public long getMinerFee() {
+        return minerFee;
+    }
+
+    public void setMinerFee(long minerFee) {
+        this.minerFee = minerFee;
     }
 }
