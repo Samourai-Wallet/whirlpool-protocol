58e03ed0b2a58a7872c04d222c0283bf129d36bb
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2018-08-31 17:23:21 +0200

    protocol upgrade: (security) use receiveAddress as bordereau

--- src/main/java/com/samourai/whirlpool/protocol/rest/RegisterOutputRequest.java
+++ src/main/java/com/samourai/whirlpool/protocol/rest/RegisterOutputRequest.java
@@ -2,7 +2,6 @@ package com.samourai.whirlpool.protocol.rest;
 
 public class RegisterOutputRequest {
     public String inputsHash;
-    public String bordereau;
     public byte[] unblindedSignedBordereau;
     public String receiveAddress;
 }
--- src/main/java/com/samourai/whirlpool/protocol/websocket/messages/RevealOutputRequest.java
+++ src/main/java/com/samourai/whirlpool/protocol/websocket/messages/RevealOutputRequest.java
@@ -2,5 +2,5 @@ package com.samourai.whirlpool.protocol.websocket.messages;
 
 public class RevealOutputRequest {
     public String mixId;
-    public String bordereau;
+    public String receiveAddress;
 }
