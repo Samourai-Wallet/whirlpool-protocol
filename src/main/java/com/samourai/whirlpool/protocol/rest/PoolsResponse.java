6d84631b13f4c13faa57c8dc622965e2df9f7002
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2018-11-24 20:32:36 +0100

    protocol upgrade: add PoolsResponse.feePaymentCode

--- src/main/java/com/samourai/whirlpool/protocol/rest/PoolsResponse.java
+++ src/main/java/com/samourai/whirlpool/protocol/rest/PoolsResponse.java
@@ -2,10 +2,12 @@ package com.samourai.whirlpool.protocol.rest;
 
 public class PoolsResponse {
   public PoolInfo[] pools;
+  public String feePaymentCode;
 
   public PoolsResponse() {}
 
-  public PoolsResponse(PoolInfo[] pools) {
+  public PoolsResponse(PoolInfo[] pools, String feePaymentCode) {
     this.pools = pools;
+    this.feePaymentCode = feePaymentCode;
   }
 }
