fc441dbca849f0743fb875e1633380f042b4d82f
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2019-01-29 13:57:02 +0100

    add WhirpoolEndpoint.WS_PREFIX

--- src/main/java/com/samourai/whirlpool/protocol/WhirlpoolEndpoint.java
+++ src/main/java/com/samourai/whirlpool/protocol/WhirlpoolEndpoint.java
@@ -3,11 +3,12 @@ package com.samourai.whirlpool.protocol;
 public class WhirlpoolEndpoint {
 
   // WEBSOCKET endpoints
-  public static final String WS_CONNECT = "/ws/connect";
-  public static final String WS_REGISTER_INPUT = "/ws/registerInput";
-  public static final String WS_CONFIRM_INPUT = "/ws/confirmInput";
-  public static final String WS_REVEAL_OUTPUT = "/ws/revealOutput";
-  public static final String WS_SIGNING = "/ws/signing";
+  public static final String WS_PREFIX = "/ws/";
+  public static final String WS_CONNECT = WS_PREFIX + "connect";
+  public static final String WS_REGISTER_INPUT = WS_PREFIX + "registerInput";
+  public static final String WS_CONFIRM_INPUT = WS_PREFIX + "confirmInput";
+  public static final String WS_REVEAL_OUTPUT = WS_PREFIX + "revealOutput";
+  public static final String WS_SIGNING = WS_PREFIX + "signing";
 
   // REST endpoints
   public static final String REST_PREFIX = "/rest/";
